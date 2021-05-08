609edf06978e6f25e27db41f9f7dd7df14e88e5b
ondrodrak <andymasox@gmail.com>
2021-05-08 22:59:56 +0200

    =killbot command

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const config = require('./configfiles/config.json');
+const stats = require('./configfiles/KARLSON/STATS/ondrodrak.json');
 const client = new Discord.Client();
 const fs = require('fs').promises;
 
+// Function defining =>
 function login() {
   client.login(config.token);
 };
@@ -10,7 +12,7 @@ function login() {
 function commandConfirm() {
   console.log('Command operation completed succesfully');
 };
-
+// <= Function defining
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -22,11 +24,29 @@ client.on('message', message => {
 	}
 });
 
+// KARLSON STATS >>>>>
+/* client.on('message', message => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+  if (command === )
+*/
 client.on('message', message => {
 	if (message.content === config.prefix + 'time') {
 		var currentTime;
 		currentTime = Date();
 		message.channel.send('The current time is' + ' ' + currentTime);
+		commandConfirm();
+	}
+});
+
+client.on('message', message => {
+	if (message.content === config.prefix + 'id') {
+		var authorID;
+		authorID = message.author.id;
+		message.channel.send('Your UID is' + ' ' + authorID);
 		commandConfirm();
 	}
 });
@@ -43,7 +63,30 @@ client.on('message', message => {
     message.channel.send(message.author.id);
     commandConfirm();
   }
-})
+});
+
+function ondraAuthorCheck() {
+	
+};
+
+client.on('message', message => {
+	if (message.content === config.prefix + 'killbot') {
+		messageContents = message;
+
+		var killcmdAuthorID;
+		var ondraAuthor;
+	
+		killcmdAuthorID = message.author.id;
+
+		ondraAuthor = killcmdAuthorID.includes(config.ondraID);
+		
+		if (ondraAuthor === true) {
+			message.channel.send('Attempting to send kill PID to NodeJS...');
+			console.log('Attempting to send kill PID to NodeJS...');
+	};
+};
+});
+
 login();
 
-// Start the bot with "node ." or "node bot.js
+// Start the bot with "node ." or "node bot.js"
