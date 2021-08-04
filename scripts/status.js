1d55323c7b27ce34543257a1f76736060709a327
ondrodrak <32337790+ondrodrak@users.noreply.github.com>
2021-08-04 22:02:50 +0200

    status
    added a few lines of code to make sure it works

--- scripts/status.js
+++ scripts/status.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js');
+const config = require('./configfiles/config.json');
+const client = new Discord.Client();
+
 function botStatus(statusText, type) { // PLAYING, STREAMING, LISTENING
     client.once('ready', () => {
         client.user.setActivity(statusText,{type: type});
